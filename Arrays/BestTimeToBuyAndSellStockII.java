6c0691ac951d59aa45e5cf596d6d0397c17535a0
Ananya-0711 <ananya.vadige@gmail.com>
2026-08-11 23:10:34 +0530

    Implemented Array to know Best time to buy and sell stock

---
+++ Arrays/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class BestTimeToBuyAndSellStockII {
+
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter length of array: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        System.out.println("Enter stock prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        BestTimeToBuyAndSellStockII obj =
+                new BestTimeToBuyAndSellStockII();
+
+        int profit = obj.maxProfit(prices);
+
+        System.out.println("Maximum Profit: " + profit);
+
+        sc.close();
+    }
+}
