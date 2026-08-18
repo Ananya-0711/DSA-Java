776913c711db1085e18632aba2bad719eefca8a8
Ananya-0711 <ananya.vadige@gmail.com>
2026-08-18 19:35:40 +0530

    Implemented To Lower case for a string

---
+++ Strings/ToLowerCase.java
@@ -0,0 +1,28 @@
+package Strings;
+import java.util.Scanner;
+public class ToLowerCase {
+    public String toLowerCase(String s){
+        StringBuilder result = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch>='a' && ch<='z'){
+                result.append(ch);
+            }
+            if(ch>='A' && ch<='Z'){
+                ch=(char)(ch+32);
+                result.append(ch);
+            }
+        }
+        return result.toString();
+   }
+   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+    String a = sc.next();
+    ToLowerCase ob = new ToLowerCase();
+    String result = ob.toLowerCase(a);
+    System.out.print(result);
+
+    sc.close();
+   }
+}
